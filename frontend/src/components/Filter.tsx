0dc4356007671d0ac04b3e551ecb78108ffb623d
terminalwarlord <joybiswas389@gmail.com>
2025-03-18 23:11:43 +0600

    fixed: responsiveness added to platform buttons on Filter component

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -28,27 +28,27 @@ const Filter = () => {
                 boxShadow: "0 5px 20px 2px rgba(0, 0, 0, 0.05)"
             }}>
             <div className="flex justify-between items-center w-full">
-                <div className="flex space-x-1 items-center px-4 cursor-pointer" onClick={toggleFilter}>
+                <div className="flex space-x-1 items-center px-1 md:px-4 cursor-pointer" onClick={toggleFilter}>
                     <FilterIcon size={15} />
-                    <p>Filter</p>
+                    <p className="text-xs md:text-sm lg:text-base">Filter</p>
                 </div>
-                <div className="flex space-x-2 transition-all ease-in-out duration-400">
+                <div className="flex space-y-2 md:space-y-0 transition-all ease-in-out duration-400 flex-col md:flex-row md:space-x-2 justify-center">
                     <Button
-                        className="rounded-xl transition-all ease-in-out duration-400"
+                        className="rounded-xl cursor-pointer transition-all ease-in-out duration-400 text-xs md:text-md lg:text-base"
                         variant={`${filterCtx.type === "all" ? "default" : "outline"}`}
                         onClick={() => filterCtx.updateFilterType('all')}
                     >
                         <ArrowDownWideNarrow /> All
                     </Button>
                     <Button
-                        className="rounded-xl transition-all ease-in-out duration-400"
+                        className="rounded-xl cursor-pointer transition-all ease-in-out duration-400 text-xs md:text-md lg:text-base"
                         variant={`${filterCtx.type === "upcoming" ? "default" : "outline"}`}
                         onClick={() => filterCtx.updateFilterType('upcoming')}
                     >
                         <ArrowDown01 /> Upcoming
                     </Button>
                     <Button
-                        className="rounded-xl transition-all ease-in-out duration-400"
+                        className="rounded-xl cursor-pointer transition-all ease-in-out duration-400 text-xs md:text-md lg:text-base"
                         variant={`${filterCtx.type === "past" ? "default" : "outline"}`}
                         onClick={() => filterCtx.updateFilterType('past')}
 
@@ -61,20 +61,20 @@ const Filter = () => {
             <div className={`w-full overflow-hidden transition-all duration-900 ease-in-out ${isFilterOpen ? "opacity-100 max-h-40 my-1" : "opacity-0 max-h-0 my-0"}`}>
                 <div className="bg-slate-500/10 h-0.5 rounded-2xl w-full"></div>
                 <div className="flex justify-between items-center">
-                    <div className="my-2 space-x-1">
+                    <div className="my-2 flex flex-col space-y-2 md:space-y-0 md:space-x-2 md:flex-row ">
                         <Button
                             onClick={() => filterCtx.updatePlatform("CODEFORCES")}
-                            className={`rounded-2xl cursor-pointer ${platforms.includes("CODEFORCES") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
+                            className={`text-xs md:text-md lg:text-base rounded-2xl cursor-pointer ${platforms.includes("CODEFORCES") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
                             Codeforces
                         </Button>
                         <Button
                             onClick={() => filterCtx.updatePlatform("CODECHEF")}
-                            className={`rounded-2xl cursor-pointer ${platforms.includes("CODECHEF") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
+                            className={`text-xs md:text-md lg:text-base rounded-2xl cursor-pointer ${platforms.includes("CODECHEF") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
                             Codechef
                         </Button>
                         <Button
                             onClick={() => filterCtx.updatePlatform("LEETCODE")}
-                            className={`rounded-2xl cursor-pointer ${platforms.includes("LEETCODE") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
+                            className={`text-xs md:text-md lg:text-base rounded-2xl cursor-pointer ${platforms.includes("LEETCODE") ? "hover:bg-gray-700 hover:dark:bg-gray-400" : "bg-slate-300 text-gray-600 hover:bg-slate-400/70  dark:bg-gray-600/40 dark:text-gray-400 hover:dark:bg-gray-600/50"} `}>
                             Leetcode
                         </Button>
 
@@ -84,7 +84,7 @@ const Filter = () => {
                             className="flex items-center justify-center space-x-1 cursor-pointer"
                             onClick={() => filterCtx.resetPlatform()}
                         >
-                            <X size={15} /> <p>Reset</p>
+                            <X size={15} /> <p className="text-xs md:text-md lg:text-base">Reset</p>
                         </div>
                     </div>
                 </div>
